b5fa2d6d7dd14214fa08c198a4b2c6d83c93b83f
dunglq28 <lequangdung0803@gamil.com>
2025-02-14 23:47:35 +0700

    update ui

--- FE/Website/CapstoneProject_SP25_IPAS/src/pages/HR/Worklog/WorklogCalendar/WorklogModal/WorklogModal.tsx
+++ FE/Website/CapstoneProject_SP25_IPAS/src/pages/HR/Worklog/WorklogCalendar/WorklogModal/WorklogModal.tsx
@@ -1,4 +1,4 @@
-import { FormFieldModal, ModalForm, SelectInfo, TimePickerInfo } from "@/components";
+import { FormFieldModal, InfoField, ModalForm, TimePickerInfo } from "@/components";
 import { worklogFormFields } from "@/constants";
 import { AssignEmployee } from "@/pages";
 import { GetUser } from "@/payloads";
@@ -9,145 +9,140 @@ import { Flex, Form, Modal, Select } from "antd";
 import { useEffect, useState } from "react";
 
 type WorklogModalProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    onSave: (values: CreateWorklogRequest) => void;
+  isOpen: boolean;
+  onClose: () => void;
+  onSave: (values: CreateWorklogRequest) => void;
 };
 
 const WorklogModal = ({ isOpen, onClose, onSave }: WorklogModalProps) => {
-    const [form] = Form.useForm();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [allEmployees, setAllEmployees] = useState<GetUser[]>([]);
-    const [selectedEmployees, setSelectedEmployees] = useState<GetUser[]>([]);
-    const [cropOptions, setCropOptions] = useState<{ value: string, label: string }[]>([]);
-    const [landPlotOptions, setLandPlotOptions] = useState<{ value: string, label: string }[]>([]);
-    const [typeOptions, setTypeOptions] = useState<{ value: string, label: string }[]>([]);
-    const farmId = getFarmId();
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [allEmployees, setAllEmployees] = useState<GetUser[]>([]);
+  const [selectedEmployees, setSelectedEmployees] = useState<GetUser[]>([]);
+  const [cropOptions, setCropOptions] = useState<{ value: string; label: string }[]>([]);
+  const [landPlotOptions, setLandPlotOptions] = useState<{ value: string; label: string }[]>([]);
+  const [typeOptions, setTypeOptions] = useState<{ value: string; label: string }[]>([]);
+  const farmId = getFarmId();
 
-    const handleCancel = () => {
-        onClose();
+  const handleCancel = () => {
+    onClose();
+  };
+
+  const handleAdd = async () => {
+    const values = await form.validateFields();
+    console.log("Form values:", values);
+
+    onSave(values);
+
+    onClose();
+  };
+
+  const handleCropChange = () => {};
+
+  const handleAssignMember = () => setIsModalOpen(true);
+  const handleConfirmAssign = () => {};
+
+  useEffect(() => {
+    if (isOpen) {
+      fetchEmployees();
+      fetchCropOptions();
     }
+  }, [isOpen]);
 
-    const handleAdd = async () => {
-        const values = await form.validateFields();
-        console.log("Form values:", values);
+  const fetchEmployees = async () => {
+    const employees = await userService.getUsersByRole("Employee");
+    setAllEmployees(employees);
+  };
 
-        onSave(values);
+  const fetchCropOptions = async () => {
+    const crops = await cropService.getCropsOfFarmForSelect(farmId);
+    const formattedCropOptions = crops.map((crop) => ({
+      value: crop.cropId,
+      label: crop.cropName,
+    }));
+    setCropOptions(formattedCropOptions);
+  };
 
-        onClose();
-    }
+  return (
+    <ModalForm
+      isOpen={isOpen}
+      onClose={handleCancel}
+      isEdit={false}
+      title="Add New Worklog"
+      onSave={handleAdd}
+    >
+      <Form form={form} layout="vertical">
+        <FormFieldModal
+          label="Task Name"
+          rules={RulesManager.getWorklogNameRules()}
+          placeholder="Enter the task name"
+          name={worklogFormFields.worklogName}
+        />
+        <Flex vertical={false} gap={10}>
+          <InfoField
+            label="Crop"
+            rules={RulesManager.getCropRules()}
+            name={worklogFormFields.cropId}
+            options={cropOptions}
+            isEditing={true}
+          />
+          <InfoField
+            label="Land Plot"
+            rules={RulesManager.getLandPlotRules()}
+            name={worklogFormFields.landPlotId}
+            options={cropOptions}
+            isEditing={true}
+          />
+        </Flex>
+        <Flex vertical={false} gap={10}>
+          <InfoField
+            label="Type"
+            rules={RulesManager.getWorklogTypeRules()}
+            name={worklogFormFields.type}
+            options={cropOptions}
+            isEditing={true}
+          />
+          <InfoField
+            label="Process"
+            rules={RulesManager.getProcessRules()}
+            name={worklogFormFields.processId}
+            options={cropOptions}
+            isEditing={true}
+          />
+        </Flex>
+        <TimePickerInfo
+          label="Time"
+          name={worklogFormFields.time}
+          rules={RulesManager.getTimeRules()}
+        />
+        <AssignEmployee members={selectedEmployees} onAssign={handleAssignMember} />
 
-    const handleCropChange = () => {
-
-    }
-
-    const handleAssignMember = () => setIsModalOpen(true);
-    const handleConfirmAssign = () => {
-    };
-
-    useEffect(() => {
-        if (isOpen) {
-            fetchEmployees();
-            fetchCropOptions();
-        }
-    }, [isOpen]);
-
-    const fetchEmployees = async () => {
-            const employees = await userService.getUsersByRole("Employee");
-            setAllEmployees(employees);
-    };
-
-    const fetchCropOptions = async () => {
-            const crops = await cropService.getCropsOfFarmForSelect(farmId);
-            const formattedCropOptions = crops.map((crop) => ({
-                value: crop.cropId,
-                label: crop.cropName,
-            }));
-            setCropOptions(formattedCropOptions);
-   
-    };
-    
-    return (
-        <ModalForm
-            isOpen={isOpen}
-            onClose={handleCancel}
-            isEdit={false}
-            title="Add New Worklog"
-            onSave={handleAdd}
+        <Modal
+          title="Assign Members"
+          open={isModalOpen}
+          onOk={() => setIsModalOpen(false)}
+          onCancel={() => setIsModalOpen(false)}
         >
-            <Form form={form} layout="vertical">
-                <FormFieldModal
-                    label="Task Name"
-                    rules={RulesManager.getWorklogNameRules()}
-                    placeholder="Enter the task name"
-                    name={worklogFormFields.worklogName}
-                />
-                <Flex vertical={false} gap={10}>
-                    <SelectInfo
-                        label="Crop"
-                        rules={RulesManager.getCropRules()}
-                        name={worklogFormFields.cropId}
-                        options={cropOptions}
-                        isEditing={true}
-                    />
-                    <SelectInfo
-                        label="Land Plot"
-                        rules={RulesManager.getLandPlotRules()}
-                        name={worklogFormFields.landPlotId}
-                        options={cropOptions}
-                        isEditing={true}
-                    />
-                </Flex>
-                <Flex vertical={false} gap={10}>
-                    <SelectInfo
-                        label="Type"
-                        rules={RulesManager.getWorklogTypeRules()}
-                        name={worklogFormFields.type}
-                        options={cropOptions}
-                        isEditing={true}
-                    />
-                    <SelectInfo
-                        label="Process"
-                        rules={RulesManager.getProcessRules()}
-                        name={worklogFormFields.processId}
-                        options={cropOptions}
-                        isEditing={true}
-                    />
-                </Flex>
-                <TimePickerInfo
-                    label="Time"
-                    name={worklogFormFields.time}
-                    rules={RulesManager.getTimeRules()}
-                />
-                <AssignEmployee members={selectedEmployees} onAssign={handleAssignMember} />
-
-                <Modal
-                    title="Assign Members"
-                    open={isModalOpen}
-                    onOk={() => setIsModalOpen(false)}
-                    onCancel={() => setIsModalOpen(false)}
-                >
-                    <Form.Item name={worklogFormFields.responsibleBy}>
-                    <Select
-                        mode="multiple"
-                        style={{ width: "100%" }}
-                        placeholder="Select employees"
-                        value={selectedEmployees}
-                        
-                        onChange={setSelectedEmployees}
-                    >
-                        {allEmployees.map((employee) => (
-                            <Select.Option key={employee.userId} value={employee.userId}>
-                                {employee.fullName}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                    </Form.Item>
-                </Modal>
-                {/* <label className={style.createdBy}> <span>Created by: </span>{authData.fullName}</label> */}
-            </Form>
-        </ModalForm>
-    );
-}
+          <Form.Item name={worklogFormFields.responsibleBy}>
+            <Select
+              mode="multiple"
+              style={{ width: "100%" }}
+              placeholder="Select employees"
+              value={selectedEmployees}
+              onChange={setSelectedEmployees}
+            >
+              {allEmployees.map((employee) => (
+                <Select.Option key={employee.userId} value={employee.userId}>
+                  {employee.fullName}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Modal>
+        {/* <label className={style.createdBy}> <span>Created by: </span>{authData.fullName}</label> */}
+      </Form>
+    </ModalForm>
+  );
+};
 
 export default WorklogModal;
