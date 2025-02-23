27db7c26b592eba6da1ae8db0cc55743854d50e3
LuuAiGiao <luugiao.lm2003@gmail.com>
2025-02-23 22:07:56 +0700

    update process detail ui

--- FE/Website/CapstoneProject_SP25_IPAS/src/components/UI/FarmInfo/Section/Section.tsx
+++ FE/Website/CapstoneProject_SP25_IPAS/src/components/UI/FarmInfo/Section/Section.tsx
@@ -1,17 +1,21 @@
 import { Flex } from "antd";
 import style from "./Section.module.scss";
+import CustomButton from "../../Button/CustomButton";
+import { Icons } from "@/assets";
 
 interface SectionProps {
   title: string;
   subtitle: string;
   children: React.ReactNode;
+  actionButton?: React.ReactNode;
 }
 
-const Section: React.FC<SectionProps> = ({ title, subtitle, children }) => (
+const Section: React.FC<SectionProps> = ({ title, subtitle, children, actionButton}) => (
   <Flex className={style.contentSection}>
     <Flex className={style.contentSectionLeft}>
       <label className={style.sectionTitle}>{title}</label>
       <label className={style.subTitle}>{subtitle}</label>
+      {actionButton && <div className={style.actionButton}>{actionButton}</div>}
     </Flex>
     <Flex className={style.contentSectionRight}>{children}</Flex>
   </Flex>
