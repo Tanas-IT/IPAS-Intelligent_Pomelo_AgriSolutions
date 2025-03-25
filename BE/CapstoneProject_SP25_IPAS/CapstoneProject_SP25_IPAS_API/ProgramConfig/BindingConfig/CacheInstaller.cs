f2ea2c41890ffb4fd515126fb90d646d4c5731a2
TamBuiInHood <144221275+TamBuiInHood@users.noreply.github.com>
2025-03-25 10:29:05 +0700

    Update - plantlot

---
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_API/ProgramConfig/BindingConfig/CacheInstaller.cs
@@ -0,0 +1,35 @@
+﻿using CapstoneProject_SP25_IPAS_BussinessObject.ProgramSetUpObject;
+using StackExchange.Redis;
+
+namespace CapstoneProject_SP25_IPAS_API.ProgramConfig.BindingConfig
+{
+    //public static class CacheInstaller
+    //{
+    //    private static IDatabase _redis;
+
+    //    public static void InstallerService(this IServiceCollection services, IConfiguration configuration)
+    //    {
+    //        // Bind Redis configuration correctly
+    //        var redisConfiguration = new RedisConfiguration();
+    //        configuration.GetSection("RedisConfiguration").Bind(redisConfiguration);
+
+    //        services.AddStackExchangeRedisCache(options =>
+    //        {
+    //            options.Configuration = "localhost:6379";
+    //            options.InstanceName = "";
+    //        });
+    //        // Register RedisConfiguration as a singleton
+    //        services.AddSingleton(redisConfiguration);
+
+    //        // Skip Redis setup if it's not enabled
+    //        if (!redisConfiguration.Enabled)
+    //            return;
+    //        // Register the Redis connection multiplexer
+    //        services.AddSingleton<IConnectionMultiplexer>(provider =>
+    //            ConnectionMultiplexer.Connect(redisConfiguration.ConnectionString));
+
+    //        // Register your custom response cache service
+    //        //services.AddScoped<IResponseCacheService, ResponseCacheService>();
+    //    }
+    //}
+}
