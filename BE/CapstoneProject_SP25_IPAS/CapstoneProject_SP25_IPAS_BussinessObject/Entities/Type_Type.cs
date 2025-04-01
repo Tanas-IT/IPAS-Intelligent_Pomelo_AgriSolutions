646eaed4a14def48916c8881f78aa36ca2bdc820
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-04-01 20:30:33 +0700

    Update Type_Type.cs

--- BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_BussinessObject/Entities/Type_Type.cs
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_BussinessObject/Entities/Type_Type.cs
@@ -14,9 +14,7 @@ namespace CapstoneProject_SP25_IPAS_BussinessObject.Entities
         [Column("MasterTypeID_2")]
         public int CriteriaSetId { get; set; }
         public bool? IsActive { get; set; }
-        [ForeignKey(nameof(ProductId))]
         public virtual MasterType? Product { get; set; }
-        [ForeignKey(nameof(CriteriaSetId))]
         public virtual MasterType? CriteriaSet { get; set; }
     }
 }
