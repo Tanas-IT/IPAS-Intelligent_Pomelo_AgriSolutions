c1ce2fd62a50e7bd1d48ffb72f1808ab30d111be
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-04-11 17:33:38 +0700

    feat: Add Report

---
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_BussinessObject/RequestModel/ReportModel/WorkPerformanceRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject_SP25_IPAS_BussinessObject.RequestModel.ReportModel
+{
+    public class WorkPerformanceRequestDto
+    {
+        public int? Top { get; set; }
+        public double? Score { get; set; }
+        public string? Search {  get; set; }
+    }
+    public class WorkPerFormanceCompareDto
+    {
+        public List<int>? ListEmployee { get; set; }
+    }
+}
