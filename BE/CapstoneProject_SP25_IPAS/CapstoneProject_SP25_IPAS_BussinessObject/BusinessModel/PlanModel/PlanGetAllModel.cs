ab5e6213c3f8d38e9a826f1cb5cf295a8079e88b
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-04-24 02:14:08 +0700

    feat: increase get all plan

---
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_BussinessObject/BusinessModel/PlanModel/PlanGetAllModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject_SP25_IPAS_BussinessObject.BusinessModel.PlanModel
+{
+    public class PlanGetAllModel
+    {
+        public int PlanId { get; set; }
+        public string? Status { get; set; }
+
+        public string? PlanName { get; set; }
+
+        public string? PlanCode { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string? Frequency { get; set; }
+        public string? PlanDetail { get; set; }
+
+        public List<ForSelectedModels>? GrowthStages { get; set; }
+        public string? Progress { get; set; }
+    }
+}
