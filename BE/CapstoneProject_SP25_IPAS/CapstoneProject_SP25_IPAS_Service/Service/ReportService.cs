3bdb232f6f9622c0521fbb2aebefc416e018affd
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-04-25 02:38:09 +0700

    feat: Update report manager

--- BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_Service/Service/ReportService.cs
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_Service/Service/ReportService.cs
@@ -1211,21 +1211,17 @@ namespace CapstoneProject_SP25_IPAS_Service.Service
                 }
                 var getAllPlantOfFarm = await _unitOfWork.PlantRepository.GetAllPlantByFarmId(farmId);
                 var total = getAllPlantOfFarm.Count;
-                var deadCount = getAllPlantOfFarm.Count(p => p.IsDead == true);
-                var normalCount = total - deadCount;
-                double deadPercentage = 0;
-                double normalPercentage = 100;
 
+                var statusPercentage = new Dictionary<string, double>();
+                
                 if (total > 0)
                 {
-                    deadPercentage = Math.Round((double)deadCount / total * 100, 2);
-                    normalPercentage = 100 - deadPercentage;
-                }
-                else
-                {
-                    // Gán mặc định hoặc xử lý trường hợp không có dữ liệu
-                    deadPercentage = 0;
-                    normalPercentage = 0;
+                    statusPercentage = getAllPlantOfFarm
+                        .GroupBy(p => p.HealthStatus ?? "Unknown") // nếu Status null thì gán "Unknown"
+                        .ToDictionary(
+                            g => g.Key,
+                            g => Math.Round((double)g.Count() / total * 100, 2)
+                        );
                 }
                 var getAllCrop = await _unitOfWork.CropRepository.GetAllCropByFarmId(farmId);
                 var totalYield = getAllCrop.Sum(x => x.ActualYield);
@@ -1246,10 +1242,7 @@ namespace CapstoneProject_SP25_IPAS_Service.Service
                     {
                         totalPlants = total,
                         totalYield = totalYield,
-                        normalCount = normalCount,
-                        deadCount = deadCount,
-                        normalPercentage = normalPercentage,
-                        deadPercentage = deadPercentage
+                        statusPercentage = statusPercentage,
                     },
                     WorkOverview = groupedByStatus
                 };
