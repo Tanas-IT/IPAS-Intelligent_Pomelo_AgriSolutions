4262c825dc185d622762593ff543e39bafd861ff
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-04-26 23:12:17 +0700

    feat: add for get plan

---
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_BussinessObject/BusinessModel/PlanModel/GetPlanForSelected.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject_SP25_IPAS_BussinessObject.BusinessModel.PlanModel
+{
+    public class GetPlanForSelected
+    {
+        
+        public int? Id { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
