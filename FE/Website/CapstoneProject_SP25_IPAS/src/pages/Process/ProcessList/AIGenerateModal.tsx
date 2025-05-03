c652be64b3c3eb4ec09e1994106d6b1fe75abafe
LuuAiGiao <luugiao.lm2003@gmail.com>
2025-05-03 12:03:24 +0700

    update process with AI

--- FE/Website/CapstoneProject_SP25_IPAS/src/pages/Process/ProcessList/AIGenerateModal.tsx
+++ FE/Website/CapstoneProject_SP25_IPAS/src/pages/Process/ProcessList/AIGenerateModal.tsx
@@ -1,24 +1,28 @@
-import { Form, Input, Modal, Switch } from "antd";
+import { Flex, Form, Input, Modal, Switch } from "antd";
 import { FormFieldModal, ModalForm } from "@/components";
 import { useState } from "react";
-import { RulesManager } from "@/utils";
-import { processFormFields } from "@/constants";
+import { planTargetOptions2, RulesManager } from "@/utils";
+import { MASTER_TYPE, processFormFields } from "@/constants";
+import { useMasterTypeOptions } from "@/hooks";
 
 interface AIGenerateModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onGenerate: (processName: string, isSample: boolean) => void;
+  onGenerate: (processName: string, isSample: boolean, masterTypeId: number, planTargetInProcess: number) => void;
 }
 
 const AIGenerateModal = ({ isOpen, onClose, onGenerate }: AIGenerateModalProps) => {
   const [form] = Form.useForm();
   const [isSample, setIsSample] = useState<boolean>(false);
+  const { options: processTypeOptions } = useMasterTypeOptions(MASTER_TYPE.PROCESS, false);
 
   const handleGenerate = async () => {
-      const values = await form.validateFields();
-      onGenerate(values.processName, isSample);
-      form.resetFields();
-      setIsSample(false);
+    const values = await form.validateFields();
+    console.log("values", values);
+    
+    onGenerate(values.processName, isSample, values.masterTypeId, values.planTargetInProcess);
+    form.resetFields();
+    setIsSample(false);
   };
 
   const handleCancel = () => {
@@ -46,6 +50,25 @@ const AIGenerateModal = ({ isOpen, onClose, onGenerate }: AIGenerateModalProps) 
           rules={RulesManager.getProcessNameRules()}
           placeholder="Enter process name"
         />
+        <Flex gap={26}>
+          <FormFieldModal
+            label="Process Type"
+            name={processFormFields.masterTypeId}
+            options={processTypeOptions}
+            rules={RulesManager.getProcessTypeRules()}
+            type="select" />
+        </Flex>
+        <Flex gap={20}>
+          <FormFieldModal
+            label="Select Target"
+            name="planTargetInProcess"
+            options={planTargetOptions2}
+            rules={RulesManager.getPlanTargetRules()}
+            disable={false}
+            type="select"
+            hasFeedback={false}
+          />
+        </Flex>
         <FormFieldModal
           type="switch"
           label="Define Process Mode:"
