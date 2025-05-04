2d5086109607eb61d4abc4e22a5a2d27c930ac98
SE173453-PhuocTan <phuoctan0206@gmail.com>
2025-05-04 11:17:17 +0700

    feat: add filter employee by target

--- BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_Repository/Repository/EmployeeSkillRepository.cs
+++ BE/CapstoneProject_SP25_IPAS/CapstoneProject_SP25_IPAS_Repository/Repository/EmployeeSkillRepository.cs
@@ -2,6 +2,7 @@
 using CapstoneProject_SP25_IPAS_Common.Enum;
 using CapstoneProject_SP25_IPAS_Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,6 +45,32 @@ namespace CapstoneProject_SP25_IPAS_Repository.Repository
         {
             var getEmployeeSkill = await _context.EmployeeSkills.Where(x => x.EmployeeID == userId && x.FarmID == farmId).ToListAsync();
             return getEmployeeSkill;
+        }
+
+        public async Task<List<UserFarm>> GetListEmployeeByTarget(string? target, int farmId)
+        {
+            if(target != null)
+            {
+                target = target.ToLower();
+            }
+
+            var result = await _context.UserFarms
+                .Include(e => e.User)
+                .Include(e => e.EmployeeSkills)
+                    .ThenInclude(es => es.WorkType)
+                .Where(x => x.FarmId == farmId && x.RoleId != (int)RoleEnum.OWNER)
+                .Select(e => new
+                {
+                    Employee = e,
+                    MatchingSkill = e.EmployeeSkills
+                        .FirstOrDefault(s => s.WorkType.Target.ToLower() == target)
+                })
+                .OrderByDescending(x => x.MatchingSkill != null) // có kỹ năng được ưu tiên trước
+                .ThenByDescending(x => x.MatchingSkill != null ? x.MatchingSkill.ScoreOfSkill : 0)
+                .Select(x => x.Employee)
+                .ToListAsync();
+
+            return result;
         }
     }
 }
